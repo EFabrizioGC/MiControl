67de651a7bf5e8e294fa57a8cdc92befd85a8b9a
EFabrizioGC <fabriziocastellanos@hotmail.com>
2021-09-18 21:47:04 -0500

    Finish Landings

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('welcome');
+Route::view('/', 'welcome');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
